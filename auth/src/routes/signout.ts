60977e937a00f8458791f6ee16cf97fbb526fb6d
allu <aleksijaakkojuhani@gmail.com>
2020-09-04 10:49:10 +0300

    adding currentUser middleware

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,15 @@
+/* 
+  to sign out, send back a header that's gonna tell the browser to dump the cookie
+*/
+
 import express from 'express';
 
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-  res.send('JUUKELI');
+  req.session = null;
+
+  res.send({});
 });
 
 export { router as signoutRouter };
