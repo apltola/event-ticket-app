2ffdc3f5ec0c175271223e59502c0dac911ce57b
allu <aleksijaakkojuhani@gmail.com>
2020-09-04 08:48:12 +0300

    cookie, jwt and environment variables stuff

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -18,16 +18,29 @@ interface UserDoc extends mongoose.Document {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    require: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      // remove all dumb stuff from document before returning it in a response
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 userSchema.pre('save', async function (done) {
   // crypt users password before saving into database
