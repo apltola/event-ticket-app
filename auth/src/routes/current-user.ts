d03b6fa826acf0aef4fa6f8c9ac7b382b81e818d
allu <aleksijaakkojuhani@gmail.com>
2020-09-04 10:10:58 +0300

    current user auth logic

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,26 @@
+/*
+  check if user's request headers has cookie with valid jwt
+  if not, return null
+  if yes, return currentUser info from jwt
+  this route is needed because React App cannot access the cookie to validate auth
+*/
+
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  console.log('HALOOOOO???');
-  res.send('CURRENT_USER');
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
