50b2c8cd395c02ded6755efdfc58c80994ae7c66
allu <aleksijaakkojuhani@gmail.com>
2020-09-04 13:14:41 +0300

    test suite setup

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,31 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+let mongo: any;
+
+// before all tests start
+beforeAll(async () => {
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+// before each test starts
+beforeEach(async () => {
+  // delete all collections before each test
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+// after all test are executed
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
