ccd715842cae735f9112edbcf9631fd7bbefd55a
allu <aleksijaakkojuhani@gmail.com>
2020-09-09 17:32:29 +0300

    route setup for order service

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -2,12 +2,12 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-
 import { errorHandler, NotFoundError, currentUser } from '@allutickets/common';
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes';
-import { updateTicketRouter } from './routes/update';
+
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,12 +18,12 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
-
 app.use(currentUser);
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+
+app.use(deleteOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
 
 // handle route not found error
 app.all('*', async () => {
