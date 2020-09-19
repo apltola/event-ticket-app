c23733b5240680eeefb151d63080a210f3dafc13
allu <aleksijaakkojuhani@gmail.com>
2020-09-19 14:11:23 +0300

    update hostname in frontend config

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -3,8 +3,7 @@ import axios from 'axios';
 const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://lippujahetimulle.com',
       headers: req.headers,
     });
   } else {
