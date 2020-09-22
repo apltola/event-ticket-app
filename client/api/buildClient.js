71c48ca1e310b416914cb8f5cc13fcf57a8d446e
allu <aleksijaakkojuhani@gmail.com>
2020-09-22 15:24:24 +0300

    header updates

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL: 'http://lippujahetimulle.com',
+      //baseURL: 'http://lippujahetimulle.com',
+      //baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL:
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers,
     });
   } else {
--- client/components/header.js
+++ client/components/header.js
@@ -5,68 +5,69 @@ const Header = ({ currentUser }) => {
     <header className="app-header">
       <nav className="nav">
         <Link href="/">
-          <a>ticket_app2020</a>
+          <a className="border">ticket_app2020</a>
         </Link>
-        <div className="right-container">
-          {currentUser && (
-            <div className="auth-links">
-              <Link href="/tickets/new">
-                <a>Sell Tickets</a>
-              </Link>
-              <Link href="/orders">
-                <a>My orders</a>
-              </Link>
-              <span className="email">{currentUser.email}</span>
-              <Link href="/auth/signout">
-                <a>
-                  <button className="signout-button">Sign out</button>
-                </a>
-              </Link>
-            </div>
-          )}
-          {!currentUser && (
-            <div className="auth-links">
-              <Link href="/auth/signin">
-                <a>Sign in</a>
-              </Link>
-              <Link href="/auth/signup">
-                <a>Register</a>
-              </Link>
-            </div>
-          )}
-        </div>
+        {currentUser && (
+          <React.Fragment>
+            <Link href="/tickets/new">
+              <a>Sell Tickets</a>
+            </Link>
+            <Link href="/orders">
+              <a>My orders</a>
+            </Link>
+            <span className="email">{currentUser.email}</span>
+            <Link href="/auth/signout">
+              <a>
+                <button className="signout-button">Sign out</button>
+              </a>
+            </Link>
+          </React.Fragment>
+        )}
+        {!currentUser && (
+          <React.Fragment>
+            <div className="spacer" />
+            <Link href="/auth/signin">
+              <a className="">Sign in</a>
+            </Link>
+            <Link href="/auth/signup">
+              <a>Register</a>
+            </Link>
+          </React.Fragment>
+        )}
       </nav>
 
       <style jsx>{`
         .app-header {
-          padding: 10px;
+          border-bottom: 1px solid #e1e4e8;
+          /* background-color: #100e17; */
         }
 
-        .nav {
-          display: flex;
-          align-items: center;
-        }
-
-        a {
-          text-decoration: none;
-          color: inherit;
-        }
-
-        .right-container {
-          flex: 1;
-          display: flex;
-          justify-content: flex-end;
-        }
-        .auth-links {
+        nav {
           display: flex;
           flex-flow: row wrap;
-          justify-content: space-evenly;
-          align-items: center;
         }
 
-        .auth-links a,
-        .auth-links span {
-          margin-left: 2vw;
+        .spacer {
+          flex: 1;
+        }
+
+        nav a {
+          flex: 1;
+          text-align: center;
+          padding: 20px;
+          text-decoration: none;
+          color: inherit;
+          font-size: 20px;
+          font-weight: bold;
+          transition: all cubic-bezier(0.075, 0.82, 0.165, 1);
+        }
+
+        a:hover {
+          background-color: rgba(0, 0, 0, 0.05);
+        }
+
+        nav a.border {
+          border-right: 1px solid #e1e4e8;
         }
       `}</style>
     </header>
